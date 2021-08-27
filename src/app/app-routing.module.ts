4e66a1cb9ef1e0f3d544b1ba1dfd67017af2fd46
Mateusz Paczwa <mateusz.paczwa.programista@gmail.com>
2021-08-27 18:25:48 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
+import { CarsListComponent } from './cars/cars-list/cars-list.component';
 
-const routes: Routes = [];
+
+const APP_ROUTES: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'cars' },
+  { path: 'cars', component: CarsListComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [ RouterModule.forRoot(APP_ROUTES)
+],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
