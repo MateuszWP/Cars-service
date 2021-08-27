c3cfb140c4ab4fa4edd30553029df44324e349e4
Mateusz Paczwa <mateusz.paczwa.programista@gmail.com>
2021-08-27 17:38:44 +0200

    Created sidebar and header

---
+++ src/app/shared-module/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+
+
+@NgModule({
+  exports: [
+    HeaderComponent
+  ],
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
